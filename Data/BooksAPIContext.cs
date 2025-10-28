88a0f01ec24d549092c74e6d5081dc3ea75cbe54
Kinga <k.garbowska1@gmail.com>
2025-10-28 00:07:42 +0000

    replaced list with SQL db, used sample static list data as seed for SQL db

---
+++ Data/BooksAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using BooksAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Data
+{
+    public class BooksAPIContext : DbContext
+    {
+        public BooksAPIContext(DbContextOptions<BooksAPIContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // default parent config of this method will still run
+
+            modelBuilder.Entity<Book>().HasData( // adding my original static list data as seed data for the DB
+                new Book { Id = 1, Title = "To Kill a Mockingbird", Author = "Harper Lee", YearPublished = 1960 },
+                new Book { Id = 2, Title = "1984", Author = "George Orwell", YearPublished = 1949 },
+                new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", YearPublished = 1925 }
+            );
+        }
+                
+
+        // making my model class available as a DB Set
+        public DbSet<BooksAPI.Models.Book> Books { get; set; }
+    }
+}
---
+++ Migrations/20251028000608_Book Data Added.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BooksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookDataAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, "Harper Lee", "To Kill a Mockingbird", 1960 },
+                    { 2, "George Orwell", "1984", 1949 },
+                    { 3, "F. Scott Fitzgerald", "The Great Gatsby", 1925 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}

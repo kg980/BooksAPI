749ab3db2ab00088b98ab66951616b6badfb8223
Kinga <k.garbowska1@gmail.com>
2025-10-29 20:05:13 +0000

    Added swagger UI, only works via https

--- Program.cs
+++ Program.cs
@@ -6,17 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+//builder.Services.UseOpenApi();
+
 
 builder.Services.AddDbContext<BooksAPI.Data.BooksAPIContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
+
 
 app.UseHttpsRedirection();
 
